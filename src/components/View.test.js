f1a2b2596f04621fe213fca0ead77f76aa610baf
ConstanceNwaigwe <constancenwaigwe@gmail.com>
2021-10-11 02:50:33 -0400

    added tests

--- src/components/View.test.js
+++ src/components/View.test.js
@@ -3,10 +3,12 @@ import { render, screen, waitFor } from '@testing-library/react';
 import View from './View';
 
 test("renders zero articles without errors", async () => {
+    render(<View/>)
 });
 
-// test("renders three articles without errors", async ()=> {
-// });
+test("renders three articles without errors", async ()=> {
+    render(<View/>)
+});
 
 //Task List
 //1. Complete the above two tests. Make sure to mocking the articleService call before rendering.
